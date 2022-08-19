9ef2f7adda3086c48eef2f5378c665c9774a9930
zhafner <104408611+zhafner@users.noreply.github.com>
2022-08-18 20:32:08 -0400

    Update APIUrl.js

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,3 +1,7 @@
-const APIUrl = "http://localhost:3001";
+let APIUrl = "http://localhost:3001";
+
+if (window.location.host.indexOf('.herokuapp.com') !== -1) {
+    APIUrl = "https://zhafner-capstone-backend.herokuapp.com";
+}
 
 export default APIUrl;
