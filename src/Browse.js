2c7c8306e45305aa565228a071cba1a7b41dbdef
zhafner <104408611+zhafner@users.noreply.github.com>
2022-08-30 20:33:28 -0400

    Update Browse.js

--- src/Browse.js
+++ src/Browse.js
@@ -1,16 +1,48 @@
+import { useState } from "react";
+
 const Browse = ()=>{
+  
+      const [titleToSearchFor, setTitleToSearchFor] = useState("");
+      const [searchResults, setSearchResults] = useState([]);
+
+
+  const Search = async (evt) => {
+    evt.preventDefault();
+
+    try {
+      const response = await fetch(`https://www.themoviedb.org/search?query=${titleToSearchFor}`);
+      const data = await response.json();
+      console.log(data)
+      // setSearchResults(data.)
+      //set data results into searchResults in state
+    } catch (err){
+      console.error(err)
+    }
+  }
+
     return (
         <div id="body">
 
     <nav className="navbar navbar-light bg-light">
     <div className="container-fluid">
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <form className="d-flex"
+      //add onSubmit function
+      onSubmit={Search} 
+      >
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"
+        value={titleToSearchFor}
+        onChange={(evt)=>{
+          setTitleToSearchFor(evt.target.value);
+        }}
+        />
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </nav>
+{/* add map over searchRestlts to show all titles */}
+        arr.map((value, index) =. {
 
+        });
 
     </div> 
 
